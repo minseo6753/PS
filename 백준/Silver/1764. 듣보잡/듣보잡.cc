f5012af770155941b8f495d18e7e8f4417fd80d0
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2025-03-30 18:56:17 +0900

    [Silver IV] Title: 듣보잡, Time: 28 ms, Memory: 9912 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,5 +1,4 @@
 #include<iostream>	
-#include<set>
 #include<vector>
 #include<algorithm>
 using namespace std;
@@ -12,24 +11,46 @@ int main() {
 
 	int n, m;
 	cin >> n >> m;
-
-	set<string>s;
+	
+	vector<string>v(n);
 	for (int i = 0; i < n; i++) {
-		string a;
-		cin >> a;
-		s.insert(a);
-	}
-	vector<string>v;
-	for (int i = 0; i < m; i++) {
-		string a;
-		cin >> a;
-		if (s.find(a)!=s.end()) {
-			v.push_back(a);
-		}
+		cin >> v[i];
 	}
 	sort(v.begin(), v.end());
-	cout << v.size();
-	for (int i = 0; i < v.size(); i++) {
-		cout << '\n' <<  v[i];
+
+	vector<string>v2(m);
+	for (int i = 0; i < m; i++) {
+		cin >> v2[i];
+	}
+	sort(v2.begin(), v2.end());
+
+	
+	vector<string>v3;
+	int st = 0;
+	int end = n - 1;
+	int i=0;
+	while(st<=end&&i<m) {
+		
+		int mid= (st + end) / 2;
+		while (st <= end) {
+			mid = (st + end) / 2;
+			if (v[mid] == v2[i]) {
+				v3.push_back(v2[i]);
+				mid++;
+				break;
+			}
+			if (v[mid] < v2[i]) {
+				st = mid + 1;
+			}
+			else end = mid - 1;
+		}
+		st = mid;
+		end = n - 1;
+		i++;
+	}
+	
+	cout << v3.size();
+	for (int i = 0; i < v3.size(); i++) {
+		cout << '\n' << v3[i];
 	}
 }

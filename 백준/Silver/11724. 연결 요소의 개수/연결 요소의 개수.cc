0839c0467e1f305034902490896355a14c05f1ac
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2025-04-12 17:30:08 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 260 ms, Memory: 6384 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<int>* adj = new vector<int>[n+1];
+	int u, v;
+	while (m--) {
+		
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	vector<bool>visit(n+1,0);
+	queue<int>q;
+	int cnt = 0;
+	
+	for (int i = 1; i <= n; i++) {
+		if (!visit[i]) {
+			cnt++;
+			q.push(i);
+			visit[i] = 1;
+
+			while (q.size()) {
+				int node = q.front();
+				q.pop();
+				for (int j = 0; j < adj[node].size(); j++) {
+					if (!visit[adj[node][j]]) {
+						q.push(adj[node][j]);
+						visit[adj[node][j]] = 1;
+					}
+				}
+			}
+			
+		}
+	}
+	
+	cout << cnt;
+
+}

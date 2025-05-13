17c3afdc43749e4b281049c286d9e6838547caf2
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2025-05-13 18:14:48 +0900

    [Bronze III] Title: 주차 요금 정산하기, Time: 0 ms, Memory: 2020 KB, Score: 7 point -BaekjoonHub

---
+++ 백준/Bronze/33753. 주차 요금 정산하기/주차 요금 정산하기.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	int t;
+	cin >> t;
+	if (t <= 30)cout << a;
+	else cout << a + (ceil(double(t-30) / b)) * c;
+}

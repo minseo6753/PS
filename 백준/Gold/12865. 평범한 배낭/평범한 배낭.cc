bb2c0b3f0119695f670bd0f0f5c58f7641fabdfd
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2025-07-13 23:08:45 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,38 +1,21 @@
 #include<iostream>
-#include<memory.h>
 using namespace std;
-
-int dp[101][100001];
-
-struct obj {
-	int weight;
-	int value;
-};
-
-obj li[101];
 
 int main() {
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	memset(dp, 0, sizeof(dp));
-
 	int n, k;
 	cin >> n >> k;
 
-	for (int i = 1; i <= n; i++) {
-		cin >> li[i].weight >> li[i].value;
-	}
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			if (li[i].weight <= j) {
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - li[i].weight] + li[i].value);
-			}
-			else dp[i][j] = dp[i - 1][j];
+	int dp[100001] = { 0 };
+	while (n--) {
+		int w, v;
+		cin >> w >> v;
+		for (int j = k; j >= w; j--) {
+			if (dp[j] < dp[j - w] + v)dp[j] = dp[j - w] + v;
 		}
 	}
-
-	cout << dp[n][k];
+	cout << dp[k];
 }

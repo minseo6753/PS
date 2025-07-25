312fcd2fc441a1032bb6cf3f2ec030f6906ad5b3
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2025-07-25 19:18:07 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 148 ms, Memory: 6772 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,33 @@
+#include<iostream>	
+#include<vector>
+using namespace std;
+
+void star(vector<vector<char>>& v, int row, int col, int n) {
+	
+	if (n == 1) {
+		v[row][col] = '*';
+		return;
+	}
+	
+	for (int i = 0; i < n; i += n / 3) {
+		for (int j = 0; j < n; j += n / 3) {
+			if (i == n / 3 && j == n / 3)continue;
+			star(v, row + i, col + j, n / 3);
+		}
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector<vector<char>>v(n, vector<char>(n,' '));
+	
+	star(v, 0, 0, n);
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << v[i][j];
+		}
+		cout << '\n';
+	}
+}

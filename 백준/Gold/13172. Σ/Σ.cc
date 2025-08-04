218230a564daf629d75031adad0da88405383afe
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2025-08-05 00:05:38 +0900

    [Gold IV] Title: Σ, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/13172. Σ/Σ.cc
+++ 백준/Gold/13172. Σ/Σ.cc
@@ -2,17 +2,6 @@
 using namespace std;
 #define X 1000000007
 
-int gcd(int a, int b) {
-
-	while (b) {
-		int r = a % b;
-		a = b;
-		b = r;
-	}
-
-	return a;
-}
-
 int fastPow(int base, int exp, int mod) {
 
 	long long result = 1;
@@ -33,17 +22,10 @@ int main() {
 		int n, s;
 		cin >> n >> s;
 
-		int g = gcd(n, s);
-		n /= g;
-		s /= g;
 
-		if (n == 1) {
-			sum += s;
-		}
-		else {
-			int inverse = fastPow(n, X - 2, X);
-			sum += (long long)s * inverse % X;
-		}
+		int inverse = fastPow(n, X - 2, X);
+		sum += (long long)s * inverse % X;
+		
 		sum %= X;
 	}
 	cout << sum;

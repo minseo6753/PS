85de6f5aeb8f1b713af7b1beef6f1aa7b0d37509
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2025-08-19 17:20:36 +0900

    [Gold II] Title: 트리의 지름, Time: 68 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct Child {
+	int num;
+	int weight;
+};
+
+struct Value {
+	int maxi;
+	int sum;
+};
+
+bool visit[100001] = { 0 };
+
+void cal(int num, vector<vector<Child>>& tree, vector<Value>& v) {
+	visit[num] = true;
+
+	int maxi1 = 0;
+	int maxi2 = 0;
+	for (int i = 0; i < tree[num].size(); i++) {
+
+		int child = tree[num][i].num;
+		int weight = tree[num][i].weight;
+
+		if (visit[child])continue;
+
+		cal(child, tree, v);
+
+		if (maxi2 < v[child].maxi + weight) {
+			if (maxi1 < v[child].maxi + weight) {
+				maxi2 = maxi1;
+				maxi1 = v[child].maxi + weight;
+			}
+			else maxi2 = v[child].maxi + weight;
+		}
+
+
+	}
+	v[num].maxi = maxi1;
+	v[num].sum = maxi1 + maxi2;
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<vector<Child>>tree(n + 1);
+	for (int i = 0; i < n; i++) {
+		int p;
+		cin >> p;
+		while (true) {
+			int c;
+			cin >> c;
+			if (c == -1)break;
+			int w;
+			cin >> w;
+			tree[p].push_back({ c,w });
+		}
+	}
+
+	vector<Value>v(n + 1);
+
+	cal(1, tree, v);
+
+	int result = 0;
+	for (int i = 1; i <= n; i++) {
+		result = max(result, v[i].sum);
+	}
+	cout << result;
+}
